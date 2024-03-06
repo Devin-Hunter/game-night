e94f768ef5301590ed59f24c72d563feba36c6e3
Julia Shvets <shvetcjulia@gmail.com>
2024-03-06 00:34:17 +0000

    Test julia

--- api/queries/games.py
+++ api/queries/games.py
@@ -90,7 +90,7 @@ class GameRepo:
                             rules, picture, video,
                             complexity, category, rating
                         FROM games
-                        ORDER BY title;
+                        ORDER BY id;
                         """
                     )
                     return [
@@ -117,3 +117,142 @@ class GameRepo:
             category=record[12],
             rating=record[13],
         )
+
+    def get_game_details(self, game_id: int) -> Optional[GameOut]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT id, title, year, min_players, max_players,
+                            play_time, age, description,
+                            rules, picture, video,
+                            complexity, category, rating
+                        FROM games
+                        WHERE id = %s
+                        """,
+                        [game_id],
+                    )
+                    record = result.fetchone()
+                    print(record)
+                    if record is None:
+                        return None
+                    return self.record_to_game_out(record)
+        except Exception as e:
+            print(e)
+            return {"message": "Could not get the detail of this game"}
+
+    def update_game(self, game_id: int, game: GameIn) -> Union[GameOut, Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        UPDATE games
+                        SET title = %s
+                        , year = %s
+                        , min_players = %s
+                        , max_players = %s
+                        , play_time = %s
+                        , age = %s
+                        , description = %s
+                        , rules = %s
+                        , picture = %s
+                        , video = %s
+                        , complexity = %s
+                        , category = %s
+                        , rating = %s
+                        WHERE id = %s
+                        """,
+                        [
+                            game.title,
+                            game.year,
+                            game.min_players,
+                            game.max_players,
+                            game.play_time,
+                            game.age,
+                            game.description,
+                            game.rules,
+                            game.picture,
+                            game.video,
+                            game.complexity,
+                            game.category,
+                            game.rating,
+                            game_id,
+                        ],
+                    )
+                    old_data = game.dict()
+                    return GameOut(id=game_id, **old_data)
+        except Exception as e:
+            print(e)
+            return {"message": "Could not update the chosen game"}
+
+    def delete_game(self, game_id: int) -> bool:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        DELETE FROM games
+                        WHERE id = %s
+                        """,
+                        [game_id],
+                    )
+                    return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def add_game_to_owned(
+        self, member_id: int, game_id: int
+    ) -> Union[bool, Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        INSERT INTO owned_games (member_id, game_id)
+                        VALUES (%s, %s)
+                        """,
+                        [member_id, game_id],
+                    )
+                    return True
+        except Exception as e:
+            print(e)
+            return {"message": "Could not add game to owned games list"}
+
+    def add_game_to_wishlist(
+        self, member_id: int, game_id: int
+    ) -> Union[bool, Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        INSERT INTO wishlist_games (member_id, game_id)
+                        VALUES (%s, %s)
+                        """,
+                        [member_id, game_id],
+                    )
+                    return True
+        except Exception as e:
+            print(e)
+            return {"message": "Could not add game to want to play list"}
+
+    def add_game_to_favorites(
+        self, member_id: int, game_id: int
+    ) -> Union[bool, Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        INSERT INTO favorite_games (member_id, game_id)
+                        VALUES (%s, %s)
+                        """,
+                        [member_id, game_id],
+                    )
+                    return True
+        except Exception as e:
+            print(e)
+            return {"message": "Could not add game to favorites list"}
--- journals/julia_shvets.md
+++ journals/julia_shvets.md
@@ -1,31 +1,18 @@
 https://stackedit.io/app#
 
-Week 13 Day 4:
-
--   Achieved:
-    We have finished checkpoints for Week 13.
-    All completed: Wireframes, API endpoints, MVP form, the project is cloned onto local computer, docker is running, able to see:
-    -localhost:8000/docs
-    -localhost:5173/
--   Brainstorming:
-    -   Making models for status and category
-    -   Interesting names for complexity level, figuring out how to implement emoji next to name
+Week 13
+Day 4:
+We have finished checkpoints for Week 13. Completed: Wireframes, API endpoints, MVP form, the project is cloned onto local computer, docker is running, able to see:
+-localhost:8000/docs
+-localhost:5173/
 
 Week 14
-Day 1:
+Day 1: Discussed database setup. We have set on PostgreSQL.
+Day 2: Defined one to many, many to many relationships within the application.
+Day 3: Set up the database, created a successful merge request.
+Day 4: Worked on migrations, perfecting the table. Created pydantic models and started on 2 endpoints: create game and list all games.
+Day 5: Worked on create and list games endpoints. Tested their functionality on FastAPI.
 
-<<<<<<< HEAD
--   Discussed database setup. We will be going with PostgreSQL.
-    Day 2:
--   Defined one to many, many to many relationships within the application.
-    Day 3:
--   Set up the database, created a successful merge request.
-    Day 4:
--   Working on migrations and creating tables
-=======
--   Day 1: Discussed database setup. We will be going with PostgreSQL.
--   Day 2: Defined one to many, many to many relationships within the application.
--   Day 3: Set up the database, created a successful merge request.
--   Day 4: Working on migrations, perfecting the table, creating a pydantic model and starting on endpoints
--   Day 5: Working on create and list games endpoints
->>>>>>> main
+Week 15:
+Day 1: Finished all game endpoints (create game, list all games, update game, delete game, get game details), made sure everything works. Starting to plan out how to tackle frontend. Will start out with just React, down the line might need to integrate Redux.
+Day 2: Reformatted the table games_members into 3 separate tables: favorite_games, owned_games, wishlist_games. Created 3 endpoints for those table, add_to_favorites, add_to_owned, add_to_wishlist.
