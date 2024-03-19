d74f2ff6aeb49ecbf3626e415a4cc0a4438a5c8f
Devin-Matherne <devinmatherne@ymail.com>
2024-03-19 17:08:40 +0000

    New location form

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import MainPage from './MainPage'
 import NavBar from './Nav'
+import AddLocationForm from './AddLocation'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
             <div className="container">
                 <Routes>
                     <Route path="/" element={<MainPage />} />
+                    <Route path="/locations" element={<AddLocationForm />} />
                 </Routes>
             </div>
         </BrowserRouter>
