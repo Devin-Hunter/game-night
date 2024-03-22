6d2a19717614e41aeda5fc84f4425dcdf963eacf
Devin Matherne <devinmatherne@ymail.com>
2024-03-21 19:29:19 -0600

    Added float-left to navbar

--- ghi/src/Nav.jsx
+++ ghi/src/Nav.jsx
@@ -52,7 +52,7 @@ function NavBar() {
           </button>
 
           <aside
-              className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+              className="float-left fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
               aria-label="Sidebar"
           >
               <div
